d88211aad4710b1dba8afeff3370af4afe696a6a
Pavani Vithala <pavani.vithala18@gmail.com>
2019-01-13 19:56:59 -0500

    set Heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 //setup MYSQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JASW_DB_URL);
+}else{
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "rootroot",
+        database: "burgers_db"
+    }); 
+};
 
 //Make Connection
 connection.connect(function (err) {
